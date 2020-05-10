8cdca82fe5c60f2e6a80151389079a5d834863e2
Scott Gallagher <35927142+s00178222@users.noreply.github.com>
2020-05-10 16:25:45 +0100

    save and loading
    pause menu done saveing and loading done options menu done also and more

---
+++ The Emporium/Assets/Scotts/Scripts/saveing and loading data/PlayerStats.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    public decimal money = 0;
+    public int health = 100;
+    public int stamina = 100;
+    public float movementSpeed = 5;
+
+
+    public void SavePlayer()
+    {
+        SaveMangerSystem.SavePlayerData(this);
+    }
+
+    public void LoadPlayer()
+    {
+        PlayerData data = SaveMangerSystem.LoadingPlayer();
+
+        money = data.money;
+        health = data.health;
+        stamina = data.stamina;
+        movementSpeed = data.movementSpeed;
+        Vector3 position;
+        position.x = data.position[0];
+        position.y = data.position[1];
+        position.z = data.position[2];
+        transform.position = position;
+    }
+}
